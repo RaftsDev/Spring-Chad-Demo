2ae4d45f258a1cfa42ace73f552d84b04f6ceee4
Ralph <raftsdev@gmail.com>
2022-05-30 15:20:07 -0400

    Feat: Add new controller with @RequestParam annotation.

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -28,6 +29,19 @@ public class HelloWorldController {
 		theName = theName.toUpperCase();
 		
 		String result = "Yo! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		
+		
+		theName = theName.toUpperCase();
+		
+		String result = "From @RequestParam: " + theName;
 		
 		model.addAttribute("message", result);
 		
