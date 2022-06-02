49dc5b8e1eb3322d8a246a70970e0e4d1b039a78
Ralph <raftsdev@gmail.com>
2022-06-02 15:11:12 -0400

    Feat: Create CourseCodeConstraintValidator class.

---
+++ src/com/luv2code/springdemo/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+	
+	private String coursePrefix;
+	
+	@Override
+	public void initialize(CourseCode theCourseCode) {
+		// TODO Auto-generated method stub
+		coursePrefix = theCourseCode.value();
+	}
+
+	@Override
+	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+}
